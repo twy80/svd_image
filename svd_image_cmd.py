bf7ecdb33efd8b5c65b47ab6d6a5836def806d60
Tae-Woong Yoon <twy@korea.ac.kr>
2023-02-01 22:38:55 +0900

    Update svd_image_cmd.py

--- svd_image_cmd.py
+++ svd_image_cmd.py
@@ -67,7 +67,7 @@ def run_svd_image_cmd():
 
     image_shape = input_image_int.shape
 
-    channels = 1 if len(image_shape) == 2 else image_shape[2]
+    channels = 1 if len(image_shape) == 2 else 3
     # If the image is grayscale, channels = 1
 
     for i in range(channels):  # Compute the rank of each channel
